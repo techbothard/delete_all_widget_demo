7c799a1941c5957126e062e2be89aa7e06ce17f0
asd <asd@gmail.com>
2024-01-12 15:30:13 +0530

    localization add

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,14 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'app/all_show_widget/common_widget.dart';
 import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 import 'package:delete_all_widget_demo/app/common/notification.dart';
+
+import 'app/common/localization/localizations.dart';
 
 Future backgroundmessage(RemoteMessage message) async {
   print("backgroundbackgroundbackground");
@@ -60,6 +63,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        AppLocalization.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('hi', 'IN'),
+      ],
       home: CommonWidget(),
     );
   }
@@ -193,139 +206,4 @@ if pullrequirest merge in main then get code in local write
 //git diff main
 //git merge main
 
-*/
-
-//fixme: info.plist
-/*
-
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-<key>CFBundleDevelopmentRegion</key>
-<string>$(DEVELOPMENT_LANGUAGE)</string>
-<key>CFBundleExecutable</key>
-<string>$(EXECUTABLE_NAME)</string>
-<key>CFBundleIdentifier</key>
-<string>$(PRODUCT_BUNDLE_IDENTIFIER)</string>
-<key>CFBundleInfoDictionaryVersion</key>
-<string>6.0</string>
-<key>CFBundleName</key>
-<string>Ethnic Export</string>
-<key>CFBundlePackageType</key>
-<string>APPL</string>
-<key>CFBundleShortVersionString</key>
-<string>$(MARKETING_VERSION)</string>
-<key>CFBundleSignature</key>
-<string>????</string>
-<key>CFBundleURLTypes</key>
-<array>
-<dict>
-<key>CFBundleTypeRole</key>
-<string>Editor</string>
-<key>CFBundleURLSchemes</key>
-<array>
-<string>com.developer.ethnicexport</string>
-</array>
-</dict>
-<dict>
-<key>CFBundleTypeRole</key>
-<string>Editor</string>
-<key>CFBundleURLSchemes</key>
-<array>
-<string>com.googleusercontent.apps.806565206717-0c35ultdsfdsafdnflsdbne2b9hi</string>
-</array>
-</dict>
-<dict>
-<key>CFBundleTypeRole</key>
-<string>Editor</string>
-<key>CFBundleURLSchemes</key>
-<array>
-<string>fb000000000000</string>
-</array>
-</dict>
-</array>
-<key>CFBundleVersion</key>
-<string>$(CURRENT_PROJECT_VERSION)</string>
-<key>FacebookAppID</key>
-<string>000000000000</string>
-<key>FacebookDisplayName</key>
-<string>YOUR_APP_NAME</string>
-<key>GADApplicationIdentifier</key>
-<string>ca-app-pub-1336141348317767~4640963371</string>
-<key>LSApplicationQueriesSchemes</key>
-<array>
-<string>fbapi</string>
-<string>fb-messenger-share-api</string>
-<string>whatsapp</string>
-<string>fbauth2</string>
-<string>fbshareextension</string>
-<string>itms</string>
-</array>
-<key>LSMinimumSystemVersion</key>
-<string>11.0</string>
-<key>LSRequiresIPhoneOS</key>
-<true/>
-<key>MinimumOSVersion</key>
-<string>11.0</string>
-<key>NSAppTransportSecurity</key>
-<dict>
-<key>NSAllowsArbitraryLoads</key>
-<true/>
-<key>NSAllowsArbitraryLoadsInWebContent</key>
-<true/>
-</dict>
-<key>NSAppleMusicUsageDescription</key>
-<string>when user get notified that time use</string>
-<key>NSCalendarsUsageDescription</key>
-<string>purpose for using calendar</string>
-<key>NSContactsUsageDescription</key>
-<string>user order time get contact number</string>
-<key>NSLocationAlwaysAndWhenInUseUsageDescription</key>
-<string>user get there location to the best useges</string>
-<key>NSLocationAlwaysUsageDescription</key>
-<string>Need current location access</string>
-<key>NSLocationWhenInUseUsageDescription</key>
-<string>Get Your current location for delivery</string>
-<key>NSMotionUsageDescription</key>
-<string>show the move motion data to display</string>
-<key>NSPhotoLibraryAddUsageDescription</key>
-<string>$(PRODUCT_NAME)</string>
-<key>NSPhotoLibraryUsageDescription</key>
-<string>This app requires access to the photo library because user may need to upload images of payment slip and also user can download/save images of the products from the application</string>
-<key>NSSpeechRecognitionUsageDescription</key>
-<string>user mic to speek add text are write</string>
-<key>NSUserTrackingUsageDescription</key>
-<string>This identifier will be used to deliver personalized ads to you.</string>
-<key>UIBackgroundModes</key>
-<array>
-<string>fetch</string>
-<string>remote-notification</string>
-</array>
-<key>UILaunchStoryboardName</key>
-<string>LaunchScreen</string>
-<key>UIMainStoryboardFile</key>
-<string>Main</string>
-<key>UISupportedInterfaceOrientations</key>
-<array>
-<string>UIInterfaceOrientationPortrait</string>
-</array>
-<key>UISupportedInterfaceOrientations~ipad</key>
-<array>
-<string>UIInterfaceOrientationPortrait</string>
-<string>UIInterfaceOrientationPortraitUpsideDown</string>
-<string>UIInterfaceOrientationLandscapeLeft</string>
-<string>UIInterfaceOrientationLandscapeRight</string>
-</array>
-<key>UIViewControllerBasedStatusBarAppearance</key>
-<false/>
-<key>io.flutter.embedded_views_preview</key>
-<string>YES</string>
-<key>io.flutter.embedded_views_preview</key>
-<true/>
-<key>FirebaseAppDelegateProxyEnabled</key>
-<false/>
-
-</dict>
-</plist>
 */
