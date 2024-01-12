49482350ce517f03823dffefcd853326f1b480c8
asd <asd@gmail.com>
2024-01-12 12:37:35 +0530

    all DONE

--- lib/main.dart
+++ lib/main.dart
@@ -194,3 +194,138 @@ if pullrequirest merge in main then get code in local write
 //git merge main
 
 */
+
+//fixme: info.plist
+/*
+
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+<key>CFBundleDevelopmentRegion</key>
+<string>$(DEVELOPMENT_LANGUAGE)</string>
+<key>CFBundleExecutable</key>
+<string>$(EXECUTABLE_NAME)</string>
+<key>CFBundleIdentifier</key>
+<string>$(PRODUCT_BUNDLE_IDENTIFIER)</string>
+<key>CFBundleInfoDictionaryVersion</key>
+<string>6.0</string>
+<key>CFBundleName</key>
+<string>Ethnic Export</string>
+<key>CFBundlePackageType</key>
+<string>APPL</string>
+<key>CFBundleShortVersionString</key>
+<string>$(MARKETING_VERSION)</string>
+<key>CFBundleSignature</key>
+<string>????</string>
+<key>CFBundleURLTypes</key>
+<array>
+<dict>
+<key>CFBundleTypeRole</key>
+<string>Editor</string>
+<key>CFBundleURLSchemes</key>
+<array>
+<string>com.developer.ethnicexport</string>
+</array>
+</dict>
+<dict>
+<key>CFBundleTypeRole</key>
+<string>Editor</string>
+<key>CFBundleURLSchemes</key>
+<array>
+<string>com.googleusercontent.apps.806565206717-0c35ultdsfdsafdnflsdbne2b9hi</string>
+</array>
+</dict>
+<dict>
+<key>CFBundleTypeRole</key>
+<string>Editor</string>
+<key>CFBundleURLSchemes</key>
+<array>
+<string>fb000000000000</string>
+</array>
+</dict>
+</array>
+<key>CFBundleVersion</key>
+<string>$(CURRENT_PROJECT_VERSION)</string>
+<key>FacebookAppID</key>
+<string>000000000000</string>
+<key>FacebookDisplayName</key>
+<string>YOUR_APP_NAME</string>
+<key>GADApplicationIdentifier</key>
+<string>ca-app-pub-1336141348317767~4640963371</string>
+<key>LSApplicationQueriesSchemes</key>
+<array>
+<string>fbapi</string>
+<string>fb-messenger-share-api</string>
+<string>whatsapp</string>
+<string>fbauth2</string>
+<string>fbshareextension</string>
+<string>itms</string>
+</array>
+<key>LSMinimumSystemVersion</key>
+<string>11.0</string>
+<key>LSRequiresIPhoneOS</key>
+<true/>
+<key>MinimumOSVersion</key>
+<string>11.0</string>
+<key>NSAppTransportSecurity</key>
+<dict>
+<key>NSAllowsArbitraryLoads</key>
+<true/>
+<key>NSAllowsArbitraryLoadsInWebContent</key>
+<true/>
+</dict>
+<key>NSAppleMusicUsageDescription</key>
+<string>when user get notified that time use</string>
+<key>NSCalendarsUsageDescription</key>
+<string>purpose for using calendar</string>
+<key>NSContactsUsageDescription</key>
+<string>user order time get contact number</string>
+<key>NSLocationAlwaysAndWhenInUseUsageDescription</key>
+<string>user get there location to the best useges</string>
+<key>NSLocationAlwaysUsageDescription</key>
+<string>Need current location access</string>
+<key>NSLocationWhenInUseUsageDescription</key>
+<string>Get Your current location for delivery</string>
+<key>NSMotionUsageDescription</key>
+<string>show the move motion data to display</string>
+<key>NSPhotoLibraryAddUsageDescription</key>
+<string>$(PRODUCT_NAME)</string>
+<key>NSPhotoLibraryUsageDescription</key>
+<string>This app requires access to the photo library because user may need to upload images of payment slip and also user can download/save images of the products from the application</string>
+<key>NSSpeechRecognitionUsageDescription</key>
+<string>user mic to speek add text are write</string>
+<key>NSUserTrackingUsageDescription</key>
+<string>This identifier will be used to deliver personalized ads to you.</string>
+<key>UIBackgroundModes</key>
+<array>
+<string>fetch</string>
+<string>remote-notification</string>
+</array>
+<key>UILaunchStoryboardName</key>
+<string>LaunchScreen</string>
+<key>UIMainStoryboardFile</key>
+<string>Main</string>
+<key>UISupportedInterfaceOrientations</key>
+<array>
+<string>UIInterfaceOrientationPortrait</string>
+</array>
+<key>UISupportedInterfaceOrientations~ipad</key>
+<array>
+<string>UIInterfaceOrientationPortrait</string>
+<string>UIInterfaceOrientationPortraitUpsideDown</string>
+<string>UIInterfaceOrientationLandscapeLeft</string>
+<string>UIInterfaceOrientationLandscapeRight</string>
+</array>
+<key>UIViewControllerBasedStatusBarAppearance</key>
+<false/>
+<key>io.flutter.embedded_views_preview</key>
+<string>YES</string>
+<key>io.flutter.embedded_views_preview</key>
+<true/>
+<key>FirebaseAppDelegateProxyEnabled</key>
+<false/>
+
+</dict>
+</plist>
+*/
